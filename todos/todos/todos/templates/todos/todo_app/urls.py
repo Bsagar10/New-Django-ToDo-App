4a6f19d50f699d8ec2d381dc096e198b4b86e35b
Bsagar10 <135614000+Bsagar10@users.noreply.github.com>
2024-03-12 22:48:35 +0530

    Delete todos directory

--- todos/todos/todos/templates/todos/todo_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from todos.views import todo_list
-
-urlpatterns = [
-    path('todos/', todo_list, name='todo_list'),
-]
--- todos/todos/todos/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import TodoItem
-
-def todo_list(request):
-    todos = TodoItem.objects.all()
-    return render(request, 'todos/todo_list.html', {'todos': todos})
